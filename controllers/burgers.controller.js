1bdabe4495daa85d06c5d8ee7301038ce78d160d
Kyle Gibson <kyle.gibson22@yahoo.com>
2021-01-16 15:08:15 -0800

    added get, post and put routes

--- controllers/burgers.controller.js
+++ controllers/burgers.controller.js
@@ -3,10 +3,30 @@ const burger = require("../models/burger.js");
 const router = express.Router();
 
 router.get("/", function(req, res){
-    burger.selectAll(data =>
+    burger.selectAll(data => {
         const ob = {
             burgers: data
         };
+        res.render("index", ob);
     });
 });
 
+router.post("/api/burgers", function(req, res){
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], result => {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res){
+    const condition = "id = " + req.params.id;
+    burger.updateOne ({devoured: req.body.devoured},
+        condition, function(result){
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+module.exports = router;
